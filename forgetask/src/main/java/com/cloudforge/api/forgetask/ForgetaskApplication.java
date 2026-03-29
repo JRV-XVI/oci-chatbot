2fed08b2e7079def9b7ff8d721684c1ddf480de9
Mario <mario.fengw@gmail.com>
2026-03-29 11:08:48 -0600

    Initialize Spring Boot

---
+++ forgetask/src/main/java/com/cloudforge/api/forgetask/ForgetaskApplication.java
@@ -0,0 +1,13 @@
+package com.cloudforge.api.forgetask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForgetaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForgetaskApplication.class, args);
+	}
+
+}
