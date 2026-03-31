8faf800bc568e1d8bed2c02dd7a9628e39ec7c5a
Mario <mario.fengw@gmail.com>
2026-03-31 16:07:34 -0600

    Connected DB with wallet to Backend

--- forgetask/src/main/java/com/cloudforge/api/forgetask/ForgetaskApplication.java
+++ forgetask/src/main/java/com/cloudforge/api/forgetask/ForgetaskApplication.java
@@ -1,13 +1,27 @@
 package com.cloudforge.api.forgetask;
 
+import jakarta.annotation.PostConstruct;
+import java.sql.Connection;
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ForgetaskApplication {
 
+	@Autowired
+	private DataSource dataSource;
+
 	public static void main(String[] args) {
 		SpringApplication.run(ForgetaskApplication.class, args);
+	}
+
+	@PostConstruct
+	public void testConnection() throws Exception {
+		try (Connection conn = dataSource.getConnection()) {
+			System.out.println("Conectado a Oracle ATP: " + conn.getMetaData().getURL());
+		}
 	}
 
 }
